260ad698a41ae3dc431a2f45fdd0353aa31af4aa
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-10 23:43:18 +0800

    Day 24 Update

--- Day 24/main.py
+++ Day 24/main.py
@@ -9,7 +9,21 @@
 # # Closing a file
 # file.close()
 
-# Another way to open the file without the need to close the file
+# Another way to open and read the file without the need to close the file
 with open("Day 24\my_file.txt") as file:
     contents = file.read()
     print(contents)
+
+# Another way to write the file without the need to close the file
+
+# write
+with open("Day 24\my_file.txt", mode='w') as file:
+    file.write("\nNew text.")
+
+# append
+with open("Day 24\my_file.txt", mode='a') as file:
+    file.write("\nNew text.")
+
+# creates a new file
+with open("Day 24\new_file.txt", mode='w') as file:
+    file.write("\nNew text.")
