4bfc269dfff450d92a0a59ee74b9f6f28ec3fea2
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-19 21:01:42 +0800

    Finish Automated Quotes Sender

---
+++ Day 32/Automated Quotes Sender/main.py
@@ -0,0 +1,25 @@
+import smtplib
+import datetime as dt
+import random
+
+# Variables
+my_email = "johnivanpuayapamerica@gmail.com"
+password = "xznqqbtxetfshwuy"
+
+# Load the Quotes
+with open("Day 32\Automated Quotes Sender\quotes.txt") as file:
+    quotes = file.readlines()
+
+# Choose a random qoute
+message = f"Subject: Monday Motivational Quote\n\n{random.choice(quotes)}"
+
+# Check the Date
+date_now = dt.datetime.now()
+
+if date_now.weekday() == 0:
+    # Send the Email
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="johnivanpuayap@gmail.com", msg=message)
+
--- Day 32/date_time.py
+++ Day 32/date_time.py
@@ -2,3 +2,17 @@ import datetime as dt
 
 now = dt.datetime.now()
 print(now)
+
+year = now.year
+print(year)
+
+if year == 2023:
+    print("Hello there!")
+
+day_of_the_week = now.weekday()
+print(day_of_the_week)
+
+
+# Create a date
+date_of_birth = dt.datetime(year=1998, month=1, day=20)
+print(date_of_birth)
