4825a6cb907a853ba404031a06340104de565317
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-20 14:51:34 +0800

    Add Exercise 1

---
+++ Day 26/exercise_1.py
@@ -0,0 +1,10 @@
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# Do Not Change the code above
+
+#Write your 1 line code below:
+
+squared_numbers = [n**2 for n in numbers]
+
+# Write your code above:
+
+print(squared_numbers)
