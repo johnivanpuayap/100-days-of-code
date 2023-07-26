20974e15b9ca63bdebafe3b70d846104c7e76363
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-26 18:42:11 +0800

    Update Day 30 main

--- Day 30/main.py
+++ Day 30/main.py
@@ -16,19 +16,32 @@
 
 # Adding an Exception in a FileNotFound Error
 
-try:
-    file = open("Day 30/a_file.txt")
-    a_dictionary = {"key": "value"}
-    value = a_dictionary["key"]
-    print(value)
-except FileNotFoundError:
-    file = open("Day 30/a_file.txt", "w")
-    file.write("something")
-except KeyError as error_message:
-    print(f"The key {error_message} doesn't exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed")
+# try:
+#     file = open("Day 30/a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     value = a_dictionary["key"]
+#     print(value)
+# except FileNotFoundError:
+#     file = open("Day 30/a_file.txt", "w")
+#     file.write("something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} doesn't exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     # file.close()
+#     # print("File was closed")
+#     raise TypeError("This is an error that I made up.")
+
+
+# Using raise
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
